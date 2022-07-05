c2accd0b95b0d9c38b4e677ed15b1265f6d41857
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-07-05 17:54:53 +0900

    final

--- src/main/java/com/example/demo/controller/GoodController.java
+++ src/main/java/com/example/demo/controller/GoodController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/get")
+@RequestMapping("/good")
 public class GoodController {
 
     @GetMapping
--- src/test/java/com/example/demo/controller/GoodControllerTest.java
+++ src/test/java/com/example/demo/controller/GoodControllerTest.java
@@ -3,23 +3,25 @@ package com.example.demo.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(GoodController.class)
 class GoodControllerTest {
 
-    @MockBean
+    @Autowired
     MockMvc mvc;
 
     @Test
     void isGet() throws Exception {
 
-        mvc.perform(get("/get"))
-            .andExpect(status().is4xxClientError());
+        mvc.perform(get("/good"))
+            .andExpect(status().isOk());
     }
 }
