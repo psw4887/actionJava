acde0e4a561ad1ac9200daab3a5be0a643fad5c2
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-07-06 10:46:39 +0900

    deploy

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+h
+8src/test/java/com/example/demo/DemoApplicationTests.java,3\a\3a91846c75497f47092961a0e4c38ac82a456e51
+q
+Asrc/test/java/com/example/demo/controller/GoodControllerTest.java,b\f\bf0741901ce8cfe9ef190e26a64fa393245711c3
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
--- src/main/java/com/example/demo/controller/GoodController.java
+++ src/main/java/com/example/demo/controller/GoodController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-//
+
 @Controller
 @RequestMapping("/good")
 public class GoodController {
@@ -13,4 +13,4 @@ public class GoodController {
 
         return "get";
     }
-}//
+}
